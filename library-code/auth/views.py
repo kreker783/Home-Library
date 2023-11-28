f85c1d5bd3e316df6f7c117e3f90daf65cab999d
Edhard <kreker783@gmail.com>
2023-11-28 23:17:10 +0100

    Start build user management

--- library-code/auth/views.py
+++ library-code/auth/views.py
@@ -7,3 +7,7 @@ class LoginPageView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'auth/login.html')
+
+
+    def post(self, request, *args, **kwargs):
+        
