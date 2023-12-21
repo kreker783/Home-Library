c9f7632e6335c8ef73e7f63f2a1828cd85f4bef8
Edhard <kreker783@gmail.com>
2023-12-21 14:22:10 +0100

    Added next/pevious pages links, need to correct url generation

--- library-code/pages/views.py
+++ library-code/pages/views.py
@@ -31,8 +31,17 @@ class CatalogPageView(View):
         search_query = request.GET.get('search_query', '')
         search_query = search_query.replace(' ', '+')
 
+        start_index = int(request.GET.get('start_index', 0))
+        max_results = int(request.GET.get('max_results', 10))
+
+        params = {
+            "q": search_query,
+            "startIndex": start_index,
+            "maxResults": max_results
+        }
+
         if search_query:
-            api_response = get_api(search_query)
+            api_response = get_api(params)
 
             for value in api_response.get('items', []):
                 volume_info = value.get("volumeInfo", {})
@@ -45,13 +54,19 @@ class CatalogPageView(View):
                     ]
                 )
 
-        print(result)
+        context = {
+            'result': result,
+            "start_index": start_index,
+            "max_results": max_results,
+            "next_start_index": start_index + max_results,
+            "previous_start_index": start_index - max_results
+        }
 
-        return render(request, template_name="pages/catalog.html", context={'result': result})
+        return render(request, template_name="pages/catalog.html", context=context)
 
 
-def get_api(q):
-    api = f"https://www.googleapis.com/books/v1/volumes?q={q}"
-    data = requests.get(api)
+def get_api(params=None):
+    api = f"https://www.googleapis.com/books/v1/volumes"
+    data = requests.get(api, params=params)
     data = data.json()
     return data
