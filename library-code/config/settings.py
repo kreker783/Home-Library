60bfa966005fc5e4c53485cb96063fa68c18a445
Edhard <kreker783@gmail.com>
2024-03-08 12:34:55 +0100

    Add gunicorn

--- library-code/config/settings.py
+++ library-code/config/settings.py
@@ -26,12 +26,16 @@ environ.Env.read_env(env_file=env_path)
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)+3pshviox==zg-9-rw25hmsf_o(@++4)j-(55orol$)tvqlne'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['ec2-13-60-42-231.eu-north-1.compute.amazonaws.com', '13.60.42.231']
+ALLOWED_HOSTS = [
+    'ec2-13-60-42-231.eu-north-1.compute.amazonaws.com',
+    '13.60.42.231',
+    '127.0.0.1'
+]
 
 
 # Application definition
